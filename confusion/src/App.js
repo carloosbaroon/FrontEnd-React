792526b3cebcd90ce5d906d97527fcc61fb68118
Carlos Baron <carlostectol@gmail.com>
2020-09-25 10:34:57 -0500

    Components Part 1
    Deleting App.css content

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './App.css';
 import Navbar from "reactstrap/lib/Navbar";
 import NavbarBrand from "reactstrap/lib/NavbarBrand";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color= "primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+        <Menu/>
     </div>
   );
 }
