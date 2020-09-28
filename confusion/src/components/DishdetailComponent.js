116a5481d88020793c09c624fa7de08fb325def0
Carlos Baron <carlostectol@gmail.com>
2020-09-27 21:19:59 -0500

    Detail is now working
    Reverting manual to funcional components commit

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,67 +1,68 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
 
-class DishDetail extends Component{
-
-    renderDish(dish){
-        if(dish != null){
-            return(
-                <Card>
-                    <CardImg width object src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle heading>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        else {
-            return(
-                <div></div>
-            )
-        }
+function RenderDish({dish}){
+    if(dish != null){
+        return(
+            <Card>
+                <CardImg width object src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle heading>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )
     }
-
-    renderComments(dish){
-        console.log('COMMENTS!!!!!!');
-        if(dish != null) {
-            const comments = dish.comments.map((comment) => {
-                return (
-                    <div key={comment.id}>
-                        <ul className="list-unstyled">
-                            <li>{comment.comment}</li>
-                            <li className="list-inline-item">{comment.author}</li>
-                            <li className="list-inline-item">{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                        </ul>
-                    </div>
-                );
-            });
-            return (
-                <div className="">
-                    <h4>Comments</h4>
-                    {comments}
-                </div>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
-    }
-
-    render() {
-        const dish = this.props.dish;
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">{this.renderDish(dish)}</div>
-                    <div className="col-12 col-md-5 m-1">{this.renderComments(dish)}</div>
-                </div>
-            </div>
-        );
-
+    else {
+        return(
+            <div></div>
+        )
     }
 }
 
+function RenderComments({dishComment}){
+    console.log('COMMENTS!!!!!!');
+    if(dishComment != null) {
+        const comments = dishComment.map((comment) => {
+            return (
+                <div key={comment.id}>
+                    <ul className="list-unstyled">
+                        <li>{comment.comment}</li>
+                        <li className="list-inline-item">{comment.author}</li>
+                        <li className="list-inline-item">{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                    </ul>
+                </div>
+            );
+        });
+        return (
+            <div className="">
+                <h4>Comments</h4>
+                {comments}
+            </div>
+        );
+    }
+    else {
+        return(
+            <div></div>
+        );
+    }
+}
+
+const DishDetail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1"><RenderDish dish={props.dish}/></div>
+                    <div className="col-12 col-md-5 m-1"><RenderComments dishComment={props.dish.comments}/></div>
+                </div>
+            </div>
+        );
+    }
+    else {
+        return(
+            <div></div>
+        );
+    }
+}
 export default DishDetail;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,31 +1,35 @@
-import React, {Component} from 'react';
-
-
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width object src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle heading>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+//Funtional component
+function RenderMenuItem({dish, onClick}){
+    return(
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width object src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle heading>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+//Another way to declare Funtional component
+const Menu = (props) =>{
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container">
-                <div className="row">{menu}</div>
-                {/*{this.renderDish(this.state.selectedDish)};*/}
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} onClick={props.onClick}/>
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">{menu}</div>
+            {/*{this.renderDish(this.state.selectedDish)};*/}
+        </div>
+    );
 }
+
+
 
 export default Menu;
