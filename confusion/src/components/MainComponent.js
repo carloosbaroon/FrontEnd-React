04236130f78ca63e9a9969d45c1326a4669b0913
Carlos Baron <carlostectol@gmail.com>
2020-09-29 19:23:25 -0500

    SPA part 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,12 +3,14 @@ import Menu from './MenuComponent';
 import Home from "./HomeComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import DishDetail from "./DishdetailComponent";
 import Contact from "./ContactComponent";
 import { DISHES } from '../shared/dishes';
 import {COMMENTS} from "../shared/comments";
 import {PROMOTIONS} from "../shared/promotions";
 import {LEADERS} from "../shared/leaders";
 import {Switch, Route, Redirect} from "react-router-dom";
+
 
 class Main extends Component {
 
@@ -33,12 +35,21 @@ class Main extends Component {
                 />
             );
         }
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+        }
+
         return (
             <div>
                 <Header/>
                 <Switch>
                     <Route path="/home" component={HomePage}/>
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId}/>
                     <Route exact path='/contactus' component={Contact}/>
                     <Redirect to="/home"/>
                 </Switch>
