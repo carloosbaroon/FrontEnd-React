467ed3a838a933152641f8b55da45897dc5ce07d
Carlos Baron <carlostectol@gmail.com>
2020-10-06 11:13:34 -0500

    Combine Reducers

---
+++ confusion/src/redux/comments.js
@@ -0,0 +1,9 @@
+import {COMMENTS} from "../shared/comments";
+
+//Reducer function
+export const Comments = (state = COMMENTS, action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+}
