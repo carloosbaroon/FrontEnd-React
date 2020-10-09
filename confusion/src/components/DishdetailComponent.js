4f9ad4aac62e5c32cb643b0e52779985ea9c16fc
Carlos Baron <carlostectol@gmail.com>
2020-10-08 19:12:09 -0500

    Redux Thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 } from "reactstrap";
 import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
+import {Loading} from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -155,6 +156,24 @@ function RenderComments({dishComment, addComment, dishId}){
 }
 
 const DishDetail = (props) => {
+    if(props.isLoading){
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading/>
+                </div>
+            </div>
+        );
+    }
+    else if(props.errMess){
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
     if (props.dish != null) {
         return (
             <div className="container">
--- confusion/src/redux/ActionCreators.js
+++ confusion/src/redux/ActionCreators.js
@@ -1,6 +1,7 @@
 import * as ActionTypes from "./ActionTypes";
+import { DISHES } from '../shared/dishes';
 
-//Funtion that creates actionObject that recieves 4 parameters
+//Function that creates actionObject that recives 4 parameters
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
@@ -9,4 +10,26 @@ export const addComment = (dishId, rating, author, comment) => ({
         author: author,
         comment: comment
     }
+});
+//Thunk, returns dispatch as a function
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    },2000)
+}
+
+export const dishesLoading = () => ({
+   type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
 });
