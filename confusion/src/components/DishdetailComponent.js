7b01df1a96edb893c8a04ceb12762a0f844bb4e8
Carlos Baron <carlostectol@gmail.com>
2020-10-13 10:11:46 -0500

    React animations components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
 import {Loading} from "./LoadingComponent";
 import {baseUrl} from "../shared/baseUrl";
+import { FadeTransform, Fade } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -111,13 +112,19 @@ class CommentForm extends Component{
 function RenderDish({dish}){
     if(dish != null){
         return(
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle heading>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle heading>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         )
     }
     else {
@@ -132,13 +139,15 @@ function RenderComments({dishComment, postComment, dishId}){
     if(dishComment != null) {
         const comments = dishComment.map((comment) => {
             return (
-                <div key={comment.id}>
-                    <ul className="list-unstyled">
-                        <li>{comment.comment}</li>
-                        <li className="list-inline-item">{comment.author}</li>
-                        <li className="list-inline-item">{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                    </ul>
-                </div>
+                <Fade in>
+                    <div key={comment.id}>
+                        <ul className="list-unstyled">
+                            <li>{comment.comment}</li>
+                            <li className="list-inline-item">{comment.author}</li>
+                            <li className="list-inline-item">{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                        </ul>
+                    </div>
+                </Fade>
             );
         });
         return (
