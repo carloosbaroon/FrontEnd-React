2caf4291c3673febb871fdd8c26dcdd238e8c2b2
Carlos Baron <carlostectol@gmail.com>
2020-10-12 22:44:28 -0500

    Fetch info from server

--- confusion/src/components/AboutComponent.js
+++ confusion/src/components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import {baseUrl} from "../shared/baseUrl";
 
 
 //Funtional component
@@ -8,7 +9,7 @@ function RenderLeader({leader}){
     return(
     <Media className="mt-5">
         <Media left>
-            <Media object src={leader.image} alt={leader.name}/>
+            <Media object src={baseUrl + leader.image} alt={leader.name}/>
         </Media>
         <Media body className="ml-5">
             <Media heading>
--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
 import {Loading} from "./LoadingComponent";
+import {baseUrl} from "../shared/baseUrl";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -111,7 +112,7 @@ function RenderDish({dish}){
     if(dish != null){
         return(
             <Card>
-                <CardImg width object src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle heading>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
